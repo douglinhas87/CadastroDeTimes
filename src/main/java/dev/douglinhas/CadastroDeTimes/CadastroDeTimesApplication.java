a7d85f2d6f298997c6c7003bd14d1fbfecc0392c
Douglas Ferraz <douglassfc02@gmail.com>
2025-06-02 16:58:27 -0300

    first commit

---
+++ src/main/java/dev/douglinhas/CadastroDeTimes/CadastroDeTimesApplication.java
@@ -0,0 +1,13 @@
+package dev.douglinhas.CadastroDeTimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeTimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeTimesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/douglinhas/CadastroDeTimes/CadastroDeTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.douglinhas.CadastroDeTimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
