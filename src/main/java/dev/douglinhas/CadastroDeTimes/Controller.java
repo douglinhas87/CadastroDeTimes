7b70315a63c637b6e038e1036b90d6e5df7000e4
Douglas Ferraz <douglassfc02@gmail.com>
2025-06-03 15:04:53 -0300

    Criação do Controller

---
+++ src/main/java/dev/douglinhas/CadastroDeTimes/Controller.java
@@ -0,0 +1,17 @@
+package dev.douglinhas.CadastroDeTimes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String bemVindo () {
+        return "SPORT CLUB DO RECIFE";
+    }
+
+
+
+
+}
